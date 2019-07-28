fd7c7e999eedf4a4babcd123c51bd847f4b0bf33
Mikko Saari <mikko@mikkosaari.fi>
2019-07-28 07:09:48 +0300

    New bbcode route.

--- routes/sessions.js
+++ routes/sessions.js
@@ -1,6 +1,47 @@
 const Session = require("../models/Session")
 const Game = require("../models/Game")
+const GameObject = require("../classes/Game")
 const jwt = require("../middlewares/jwt")
+const moment = require("moment")
+
+const generateDateParam = (week, month, year) => {
+	year = Number.isInteger(parseInt(year)) ? parseInt(year) : null
+	month = Number.isInteger(parseInt(month)) ? parseInt(month) : null
+	week = Number.isInteger(parseInt(week)) ? parseInt(week) : null
+
+	let dateParam = {}
+
+	if (year && month) {
+		const theMonth = moment(year + " " + month, "YYYY MM")
+
+		dateParam = {
+			date: {
+				$gte: `${theMonth.startOf("month").format("YYYY-MM-DD")}`,
+				$lte: `${theMonth.endOf("month").format("YYYY-MM-DD")}`
+			}
+		}
+		console.log(dateParam)
+	} else if (year && week) {
+		const theWeek = moment(year + "W" + week)
+		dateParam = {
+			date: {
+				$gte: `${theWeek.startOf("isoWeek").format("YYYY-MM-DD")}`,
+				$lte: `${theWeek.endOf("isoWeek").format("YYYY-MM-DD")}`
+			}
+		}
+	} else if (year) {
+		const theYear = moment(year, "YYYY")
+
+		dateParam = {
+			date: {
+				$gte: `${theYear.startOf("year").format("YYYY-MM-DD")}`,
+				$lte: `${theYear.endOf("year").format("YYYY-MM-DD")}`
+			}
+		}
+	}
+
+	return dateParam
+}
 
 module.exports = ({ sessionsRouter }) => {
 	sessionsRouter.get("/", async (ctx, next) => {
@@ -9,32 +50,14 @@ module.exports = ({ sessionsRouter }) => {
 
 		let params = {}
 
-		let year = 0
-		if (Number.isInteger(parseInt(ctx.request.query.year))) {
-			year = parseInt(ctx.request.query.year)
-		}
-		let month = 0
-		if (Number.isInteger(parseInt(ctx.request.query.month))) {
-			month = parseInt(ctx.request.query.month)
-		}
-		if (year && month) {
-			const toMonth = month === 12 ? 1 : month + 1
-			const toYear = month === 1 ? year + 1 : year
+		const dateParam = generateDateParam(
+			ctx.request.query.week,
+			ctx.request.query.month,
+			ctx.request.query.year
+		)
 
-			params = {
-				...params,
-				date: { $gte: `${year}-${month}-01`, $lt: `${toYear}-${toMonth}-01` }
-			}
-		} else if (year) {
-			const toYear = year + 1
+		if (dateParam) params = dateParam
 
-			params = {
-				...params,
-				date: { $gte: `${year}-01-01`, $lt: `${toYear}-01-01` }
-			}
-		}
-
-		let game = null
 		if (ctx.request.query.game) {
 			params = {
 				...params,
@@ -51,6 +74,108 @@ module.exports = ({ sessionsRouter }) => {
 			.limit(limit)
 
 		ctx.body = sessions.map(s => s.toJSON())
+	})
+
+	sessionsRouter.get("/bbcode/", async (ctx, next) => {
+		order = "rating"
+		if (ctx.request.query.order === "name") order = "name"
+
+		let params = {}
+
+		const dateParam = generateDateParam(
+			ctx.request.query.week,
+			ctx.request.query.month,
+			ctx.request.query.year
+		)
+
+		if (dateParam) params = dateParam
+
+		const games = await Game.find({})
+		const sessions = await Session.find(params)
+
+		const gameObjects = games.map(game => {
+			return new GameObject(game.name, game.id, game)
+		})
+
+		for (var i = 0; i < sessions.length; i++) {
+			const object = gameObjects.find(game => game.name === sessions[i].game)
+			if (object) {
+				object.addSession(sessions[i].plays, sessions[i].wins)
+			}
+			if (object.parent) {
+				const parentObject = gameObjects.find(
+					game => game.name === object.parent
+				)
+				if (parentObject) {
+					parentObject.addSession(sessions[i].plays, sessions[i].wins)
+				}
+			}
+		}
+
+		const filteredGames = gameObjects.filter(
+			game => game.plays > 0 && !game.parent
+		)
+
+		switch (order) {
+			case "rating":
+				filteredGames.sort((a, b) => b.rating - a.rating)
+				break
+			case "name":
+				filteredGames.sort((a, b) => {
+					if (a.name < b.name) return -1
+					return 1
+				})
+				break
+		}
+
+		const output = filteredGames.reduce((result, game) => {
+			let text = ""
+			console.log(game)
+			switch (game.rating) {
+				case 10:
+					text =
+						"[b][bgcolor=#00cc00][color=#00cc00].[/color][color=#000000]10[/color][color=#00cc00]![/color][/bgcolor][/b]"
+					break
+				case 9:
+					text =
+						"[b][bgcolor=#33cc99][color=#33cc99]_[/color][color=#000000]9[/color][color=#33cc99]_[/color][/bgcolor][/b]"
+					break
+				case 8:
+					text =
+						"[b][bgcolor=#66ff99][color=#66ff99]_[/color][color=#000000]8[/color][color=#66ff99]_[/color][/bgcolor][/b]"
+					break
+				case 7:
+					text =
+						"[b][bgcolor=#99ffff][color=#99ffff]_[/color][color=#000000]7[/color][color=#99ffff]_[/color][/bgcolor][/b]"
+					break
+				case 6:
+					text =
+						"[b][bgcolor=#9999ff][color=#9999ff]_[/color][color=#000000]6[/color][color=#9999ff]_[/color][/bgcolor][/b]"
+					break
+				case 5:
+					text =
+						"[b][bgcolor=#cc99ff][color=#cc99ff]_[/color][color=#000000]5[/color][color=#cc99ff]_[/color][/bgcolor][/b]"
+					break
+				case 4:
+					text =
+						"[b][bgcolor=#ff66cc][color=#ff66cc]_[/color][color=#000000]4[/color][color=#ff66cc]_[/color][/bgcolor][/b]"
+					break
+				case 3:
+					text =
+						"[b][bgcolor=#ff6699][color=#ff6699]_[/color][color=#000000]3[/color][color=#ff6699]_[/color][/bgcolor][/b]"
+					break
+				case 2:
+					text =
+						"[b][bgcolor=#ff3366][color=#ff3366]_[/color][color=#000000]2[/color][color=#ff3366]_[/color][/bgcolor][/b]"
+					break
+				default:
+					text = ""
+			}
+			result += `${game.plays} × ${text} ${game.name}\n`
+			return result
+		}, "")
+
+		ctx.body = JSON.stringify(output)
 	})
 
 	sessionsRouter.get("/:id", async (ctx, next) => {
