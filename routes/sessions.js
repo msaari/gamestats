cc5f55008ac4be1ac7366bc6055df396f7d3f17a
Mikko Saari <mikko@mikkosaari.fi>
2019-08-11 07:31:25 +0300

    Move date handling to utility.

--- routes/sessions.js
+++ routes/sessions.js
@@ -2,62 +2,7 @@ const Session = require("../models/Session")
 const Game = require("../models/Game")
 const GameObject = require("../classes/Game")
 const jwt = require("../middlewares/jwt")
-const moment = require("moment")
-
-const generateWeekDateParam = (week, month, year) => {
-	year = Number.isInteger(parseInt(year)) ? parseInt(year) : null
-	month = Number.isInteger(parseInt(month)) ? parseInt(month) : null
-	week = Number.isInteger(parseInt(week)) ? parseInt(week) : null
-
-	let dateParam = {}
-
-	if (year && month) {
-		const theMonth = moment(year + " " + month, "YYYY MM")
-
-		dateParam = {
-			date: {
-				$gte: `${theMonth.startOf("month").format("YYYY-MM-DD")}`,
-				$lte: `${theMonth.endOf("month").format("YYYY-MM-DD")}`
-			}
-		}
-		console.log(dateParam)
-	} else if (year && week) {
-		const theWeek = moment(year + "W" + week)
-		dateParam = {
-			date: {
-				$gte: `${theWeek.startOf("isoWeek").format("YYYY-MM-DD")}`,
-				$lte: `${theWeek.endOf("isoWeek").format("YYYY-MM-DD")}`
-			}
-		}
-	} else if (year) {
-		const theYear = moment(year, "YYYY")
-
-		dateParam = {
-			date: {
-				$gte: `${theYear.startOf("year").format("YYYY-MM-DD")}`,
-				$lte: `${theYear.endOf("year").format("YYYY-MM-DD")}`
-			}
-		}
-	}
-
-	return dateParam
-}
-
-const generateRangeDateParam = (from, to) => {
-	const now = moment()
-	if (!to) {
-		to = now.format("YYYY-MM-DD")
-	}
-
-	let dateParam = {
-		date: {
-			$gte: `${from}`,
-			$lte: `${to}`
-		}
-	}
-
-	return dateParam
-}
+const dates = require("../utils/dates")
 
 module.exports = ({ sessionsRouter }) => {
 	sessionsRouter.get("/", async (ctx, next) => {
@@ -66,22 +11,7 @@ module.exports = ({ sessionsRouter }) => {
 
 		let params = {}
 
-		let dateParam = null
-
-		if (ctx.request.query.week) {
-			dateParam = generateWeekDateParam(
-				ctx.request.query.week,
-				ctx.request.query.month,
-				ctx.request.query.year
-			)
-		}
-
-		if (ctx.request.query.from) {
-			dateParam = generateRangeDateParam(
-				ctx.request.query.from,
-				ctx.request.query.to
-			)
-		}
+		let dateParam = dates.readDateParam(ctx.request.query)
 		if (dateParam) params = dateParam
 
 		if (ctx.request.query.game) {
@@ -108,12 +38,7 @@ module.exports = ({ sessionsRouter }) => {
 
 		let params = {}
 
-		const dateParam = generateWeekDateParam(
-			ctx.request.query.week,
-			ctx.request.query.month,
-			ctx.request.query.year
-		)
-
+		let dateParam = dates.readDateParam(ctx.request.query)
 		if (dateParam) params = dateParam
 
 		const games = await Game.find({})
@@ -218,7 +143,7 @@ module.exports = ({ sessionsRouter }) => {
 
 		if (!body.game) ctx.throw(400, "Game not specified.")
 		if (!body.players) ctx.throw(400, "Players not specified.")
-		if (!body.wins) ctx.throw(400, "Wins not specified.")
+		if (!body.wins) body.wins = 0
 		if (!body.plays) ctx.throw(400, "Plays not specified.")
 		if (!body.date) ctx.throw(400, "Date not specified.")
 
