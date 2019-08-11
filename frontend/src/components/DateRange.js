f7e5333ba8e39537bc74104fc3c7d61eca57deb7
Mikko Saari <mikko@mikkosaari.fi>
2019-08-11 10:48:27 +0300

    Improved controls for date range.

--- frontend/src/components/DateRange.js
+++ frontend/src/components/DateRange.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import Form from "react-bootstrap/Form"
 import Col from "react-bootstrap/Col"
 import Button from "react-bootstrap/Button"
@@ -35,6 +35,63 @@ const DateRange = ({ paramSetter }) => {
 	const from = moment()
 	const to = moment()
 
+	const [defaultValues, setDefaultValues] = useState({
+		fromDay: from.startOf("month").format("D"),
+		fromMonth: from.month() + 1,
+		fromYear: from.year(),
+		toDay: to.date(),
+		toMonth: to.month() + 1,
+		toYear: to.year()
+	})
+
+	const handleInputChange = event => {
+		const target = event.target
+		const value = target.value
+		const name = target.name
+
+		setDefaultValues({
+			[name]: value
+		})
+	}
+
+	const setDate = mode => {
+		const today = moment()
+		switch (mode) {
+			case "alltime":
+				setDefaultValues({
+					fromDay: 1,
+					fromMonth: 1,
+					fromYear: 1,
+					toDay: today.date(),
+					toMonth: today.month() + 1,
+					toYear: today.year()
+				})
+				break
+			case "12months": {
+				const yearAgo = moment().subtract(1, "years")
+
+				setDefaultValues({
+					fromDay: yearAgo.date(),
+					fromMonth: yearAgo.month() + 1,
+					fromYear: yearAgo.year(),
+					toDay: today.date(),
+					toMonth: today.month() + 1,
+					toYear: today.year()
+				})
+				break
+			}
+			default:
+				setDefaultValues({
+					fromDay: from.startOf("month").format("D"),
+					fromMonth: from.month() + 1,
+					fromYear: from.year(),
+					toDay: to.date(),
+					toMonth: to.month() + 1,
+					toYear: to.year()
+				})
+		}
+	}
+
 	setDateParams = paramSetter
 
 	return (
@@ -44,16 +101,28 @@ const DateRange = ({ paramSetter }) => {
 					<Form.Label>Day</Form.Label>
 					<Form.Control
 						type="number"
-						defaultValue={from.startOf("month").format("D")}
+						onChange={handleInputChange}
+						name="fromDay"
+						value={defaultValues.fromDay}
 					/>
 				</Form.Group>
 				<Form.Group as={Col} md="1" controlId="fromMonth">
 					<Form.Label>Month</Form.Label>
-					<Form.Control type="number" defaultValue={from.month() + 1} />
+					<Form.Control
+						type="number"
+						onChange={handleInputChange}
+						name="fromMonth"
+						value={defaultValues.fromMonth}
+					/>
 				</Form.Group>
 				<Form.Group as={Col} md="2" controlId="fromYear">
 					<Form.Label>Year</Form.Label>
-					<Form.Control type="number" defaultValue={from.year()} />
+					<Form.Control
+						type="number"
+						onChange={handleInputChange}
+						name="fromYear"
+						value={defaultValues.fromYear}
+					/>
 				</Form.Group>
 				<Form.Group as={Col} md="1" controlId="to">
 					<Form.Label>&nbsp;</Form.Label>
@@ -61,15 +130,30 @@ const DateRange = ({ paramSetter }) => {
 				</Form.Group>
 				<Form.Group as={Col} md="1" controlId="toDay">
 					<Form.Label>Day</Form.Label>
-					<Form.Control type="number" defaultValue={to.date()} />
+					<Form.Control
+						type="number"
+						onChange={handleInputChange}
+						name="toDay"
+						value={defaultValues.toDay}
+					/>
 				</Form.Group>
 				<Form.Group as={Col} md="1" controlId="toMonth">
 					<Form.Label>Month</Form.Label>
-					<Form.Control type="number" defaultValue={to.month() + 1} />
+					<Form.Control
+						type="number"
+						onChange={handleInputChange}
+						name="toMonth"
+						value={defaultValues.toMonth}
+					/>
 				</Form.Group>
 				<Form.Group as={Col} md="2" controlId="toYear">
 					<Form.Label>Year</Form.Label>
-					<Form.Control type="number" defaultValue={to.year()} />
+					<Form.Control
+						type="number"
+						onChange={handleInputChange}
+						name="toYear"
+						value={defaultValues.toYear}
+					/>
 				</Form.Group>
 				<Form.Group as={Col} md="3">
 					<Form.Label>&nbsp;</Form.Label>
@@ -77,6 +161,18 @@ const DateRange = ({ paramSetter }) => {
 						Update
 					</Button>
 				</Form.Group>
+			</Form.Row>
+			<Form.Row>
+				<Col>
+					<Button onClick={() => setDate("alltime")} variant="primary">
+						All time
+					</Button>
+				</Col>
+				<Col>
+					<Button onClick={() => setDate("12months")} variant="primary">
+						12 months
+					</Button>
+				</Col>
 			</Form.Row>
 		</Form>
 	)
