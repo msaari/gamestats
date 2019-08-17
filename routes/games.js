c504e640bf19956ed870150b39186f7c63189e4b
Mikko Saari <mikko@mikkosaari.fi>
2019-08-17 14:38:24 +0300

    Remove /sessions/bbcode, move it to /games/.

--- routes/games.js
+++ routes/games.js
@@ -8,10 +8,61 @@ function escapeRegExp(text) {
 	return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&")
 }
 
+const bbCodify = games => {
+	const output = games.reduce((result, game) => {
+		let text = ""
+		switch (game.rating) {
+			case 10:
+				text =
+					"[b][bgcolor=#00cc00][color=#00cc00].[/color][color=#000000]10[/color][color=#00cc00]![/color][/bgcolor][/b]"
+				break
+			case 9:
+				text =
+					"[b][bgcolor=#33cc99][color=#33cc99]_[/color][color=#000000]9[/color][color=#33cc99]_[/color][/bgcolor][/b]"
+				break
+			case 8:
+				text =
+					"[b][bgcolor=#66ff99][color=#66ff99]_[/color][color=#000000]8[/color][color=#66ff99]_[/color][/bgcolor][/b]"
+				break
+			case 7:
+				text =
+					"[b][bgcolor=#99ffff][color=#99ffff]_[/color][color=#000000]7[/color][color=#99ffff]_[/color][/bgcolor][/b]"
+				break
+			case 6:
+				text =
+					"[b][bgcolor=#9999ff][color=#9999ff]_[/color][color=#000000]6[/color][color=#9999ff]_[/color][/bgcolor][/b]"
+				break
+			case 5:
+				text =
+					"[b][bgcolor=#cc99ff][color=#cc99ff]_[/color][color=#000000]5[/color][color=#cc99ff]_[/color][/bgcolor][/b]"
+				break
+			case 4:
+				text =
+					"[b][bgcolor=#ff66cc][color=#ff66cc]_[/color][color=#000000]4[/color][color=#ff66cc]_[/color][/bgcolor][/b]"
+				break
+			case 3:
+				text =
+					"[b][bgcolor=#ff6699][color=#ff6699]_[/color][color=#000000]3[/color][color=#ff6699]_[/color][/bgcolor][/b]"
+				break
+			case 2:
+				text =
+					"[b][bgcolor=#ff3366][color=#ff3366]_[/color][color=#000000]2[/color][color=#ff3366]_[/color][/bgcolor][/b]"
+				break
+			default:
+				text = ""
+		}
+		result += `${game.plays} × ${text} ${game.name}\n`
+		return result
+	}, "")
+	return output
+}
+
 module.exports = ({ gamesRouter }) => {
 	gamesRouter.get("/", async (ctx, next) => {
+		const validOrders = ["name", "plays", "rating"]
 		let order = "name"
-		if (ctx.request.query.order === "plays") order = "plays"
+		if (validOrders.indexOf(ctx.request.query.order))
+			order = ctx.request.query.order
 
 		let sessionParams = {}
 		let dateParam = dates.readDateParam(ctx.request.query)
@@ -72,9 +123,18 @@ module.exports = ({ gamesRouter }) => {
 					return 1
 				})
 				break
+			case "rating":
+				filteredGames.sort((a, b) => b.rating - a.rating)
+				break
 		}
 
-		ctx.body = filteredGames
+		switch (ctx.request.query.output) {
+			case "bbcode":
+				ctx.body = JSON.stringify(bbCodify(filteredGames))
+				break
+			default:
+				ctx.body = filteredGames
+		}
 	})
 
 	gamesRouter.get("/name/:name", async (ctx, next) => {
--- routes/sessions.js
+++ routes/sessions.js
@@ -1,6 +1,5 @@
 const Session = require("../models/Session")
 const Game = require("../models/Game")
-const GameObject = require("../classes/Game")
 const jwt = require("../middlewares/jwt")
 const dates = require("../utils/dates")
 
@@ -30,102 +29,6 @@ module.exports = ({ sessionsRouter }) => {
 			.limit(limit)
 
 		ctx.body = sessions.map(s => s.toJSON())
-	})
-
-	sessionsRouter.get("/bbcode/", async (ctx, next) => {
-		order = "rating"
-		if (ctx.request.query.order === "name") order = "name"
-
-		let params = {}
-
-		let dateParam = dates.readDateParam(ctx.request.query)
-		if (dateParam) params = dateParam
-
-		const games = await Game.find({})
-		const sessions = await Session.find(params)
-
-		const gameObjects = games.map(game => {
-			return new GameObject(game.name, game.id, game)
-		})
-
-		for (var i = 0; i < sessions.length; i++) {
-			const object = gameObjects.find(game => game.name === sessions[i].game)
-			if (object) {
-				object.addSession(sessions[i].plays, sessions[i].wins)
-			}
-			if (object.parent) {
-				const parentObject = gameObjects.find(
-					game => game.name === object.parent
-				)
-				if (parentObject) {
-					parentObject.addSession(sessions[i].plays, sessions[i].wins)
-				}
-			}
-		}
-
-		const filteredGames = gameObjects.filter(
-			game => game.plays > 0 && !game.parent
-		)
-
-		switch (order) {
-			case "rating":
-				filteredGames.sort((a, b) => b.rating - a.rating)
-				break
-			case "name":
-				filteredGames.sort((a, b) => {
-					if (a.name < b.name) return -1
-					return 1
-				})
-				break
-		}
-
-		const output = filteredGames.reduce((result, game) => {
-			let text = ""
-			switch (game.rating) {
-				case 10:
-					text =
-						"[b][bgcolor=#00cc00][color=#00cc00].[/color][color=#000000]10[/color][color=#00cc00]![/color][/bgcolor][/b]"
-					break
-				case 9:
-					text =
-						"[b][bgcolor=#33cc99][color=#33cc99]_[/color][color=#000000]9[/color][color=#33cc99]_[/color][/bgcolor][/b]"
-					break
-				case 8:
-					text =
-						"[b][bgcolor=#66ff99][color=#66ff99]_[/color][color=#000000]8[/color][color=#66ff99]_[/color][/bgcolor][/b]"
-					break
-				case 7:
-					text =
-						"[b][bgcolor=#99ffff][color=#99ffff]_[/color][color=#000000]7[/color][color=#99ffff]_[/color][/bgcolor][/b]"
-					break
-				case 6:
-					text =
-						"[b][bgcolor=#9999ff][color=#9999ff]_[/color][color=#000000]6[/color][color=#9999ff]_[/color][/bgcolor][/b]"
-					break
-				case 5:
-					text =
-						"[b][bgcolor=#cc99ff][color=#cc99ff]_[/color][color=#000000]5[/color][color=#cc99ff]_[/color][/bgcolor][/b]"
-					break
-				case 4:
-					text =
-						"[b][bgcolor=#ff66cc][color=#ff66cc]_[/color][color=#000000]4[/color][color=#ff66cc]_[/color][/bgcolor][/b]"
-					break
-				case 3:
-					text =
-						"[b][bgcolor=#ff6699][color=#ff6699]_[/color][color=#000000]3[/color][color=#ff6699]_[/color][/bgcolor][/b]"
-					break
-				case 2:
-					text =
-						"[b][bgcolor=#ff3366][color=#ff3366]_[/color][color=#000000]2[/color][color=#ff3366]_[/color][/bgcolor][/b]"
-					break
-				default:
-					text = ""
-			}
-			result += `${game.plays} × ${text} ${game.name}\n`
-			return result
-		}, "")
-
-		ctx.body = JSON.stringify(output)
 	})
 
 	sessionsRouter.get("/:id", async (ctx, next) => {
